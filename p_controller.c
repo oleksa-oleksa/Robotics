c510cfd0ab8a5857a4952675c36faf7b9544faec
Oleksandra Baga <oleksandra.baga@gmail.com>
2018-09-18 18:47:55 +0200

    P, PI, PD and PID controllers implemented in C

--- p_controller.c
+++ p_controller.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+/* The value of Kp needs to be chosen carefully in order 
+to get the optimum system response. 
+Lower values for Kp will tend to give smoother but slower responses. 
+
+Higher values of Kp will yield much quicker response but may cause overshoot, 
+where the output oscillates before settling.
+
+Excessively high values of Kp may even throw the loop into an unstable state 
+where the output oscillates without ever settling at the set point.
+
+As can be seen from the graph of P controller, the actual position 
+of the motor shaft, when settles down will not reach the target position. 
+This is because when the current position is near to the target position, 
+the error becomes very small and the computed PWM duty cycle is too small 
+for the motor to overcome the friction and gravity. 
+The small error that exists when the system has settled down is called the steady state error.
+
+*/
+
 int main()
 {
 	while (1) {
